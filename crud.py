0f7d29af5181b3cf6da1a6c719cfcd1584bf9887
Abdullah Mzaien <xa300600@hotmail.com>
2022-07-22 16:58:19 +0300

    chore: preparing for being public

--- crud.py
+++ crud.py
@@ -1,6 +1,4 @@
-from ast import Num
 import os
-import schemas
 from supabase import create_client, Client
 url = os.getenv("SUPABASE_URL", "https://csduwsbnhkcdxalrfcem.supabase.co")
 key = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZHV3c2JuaGtjZHhhbHJmY2VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDc3MDMxMDQsImV4cCI6MTk2MzI3OTEwNH0.h5EjQPuuHU4O30d_2EoU9_8E7l4CD9OnTWYM2DnUmQ8")
